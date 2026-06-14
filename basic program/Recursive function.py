dc02104c45da6107f9d0af1a898ddd689f9f3e1c
Anjali Thakur <anjalithakur7691@gmail.com>
2026-06-14 22:17:27 +0530

    Delete basic program directory

--- basic program/Recursive function.py
+++
@@ -1,6 +0,0 @@
-# RECURSIVE FUNCTION n! = nx(n-1)!
-def fact(n):
-    if n == 1:
-        return 1
-    return n * fact(n-1)
-print(fact(8))
--- basic program/from function import add.py
+++
@@ -1,5 +0,0 @@
-from fun import calc1
-from fun import calc2
-from fun import add
-print(add(30,60))         
-        
